fdf4ba8093c08dc1cbfbd3655d3983cec4490e8d
Timo Derstappen <teemow@gmail.com>
2013-07-13 19:53:59 +0000

    changed index.html to jade template

---
+++ public/javascripts/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  var carousel = $('#myCarousel').carousel();
+  carousel.carousel('pause');
+
+  $("input[type=radio]").click(function() {
+
+    setTimeout(function() {
+      carousel.carousel('next');
+    }, 200);
+  });
+});
+
